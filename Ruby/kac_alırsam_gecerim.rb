1a7ebc58adcd0701601ea614ed633ba5e9ba635d
Mesut Çittir <mesut.cittir@bil.omu.edu.tr>
2013-04-27 23:38:20 +0300

    update

--- Ruby/kac_alırsam_gecerim.rb
+++ Ruby/kac_alırsam_gecerim.rb
@@ -6,7 +6,12 @@ def hesapla()
   g = gets.chomp
   print "Vize notunuzu söylermisiniz :"
   v = gets.chomp
-  puts "En düşük notla geçebilmeniz için #{(60 - (0.4 * v.to_f)) / 0.6} almanız gerek.. "
+  result = (g.to_i - (0.4 * v.to_f)) / 0.6
+  if result < 50
+    puts "Normalde #{result} almanız yetiyor ama En az 50 almanız gerek.."
+  else
+    puts "En düşük notla geçebilmeniz için #{result} almanız gerek.."
+  end
 end
 
 if __FILE__ == $0
