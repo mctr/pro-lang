3d2587980b2e4dc9a3336b240e6ab37d251cdf76
Mesut Çittir <mesut.cittir@bil.omu.edu.tr>
2013-10-05 22:58:36 +0300

    array test

---
+++ GO/array_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var slice2 []int
+	var slice3 []int
+	
+	slice1 := []int{1, 2, 3}
+	slice2 = append(slice1, 4, 5)
+	slice3 = append(slice1, slice2...)
+	fmt.Println(slice3)
+}
